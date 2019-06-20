4f97c54f84f48ff813cc9c87915a347b9451caf2
teezyfortune <gabteezy@gmail.com>
2019-06-20 22:00:03 +0100

    new changes

---
+++ transaction.js
@@ -0,0 +1,16 @@
+// const path = require('path');
+// const express= require('express');
+// const 
+// const db = require('../appproject/conn');
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(express.urlencoded({extended: false}));
+
+// app.get('/users', db.getUsers);
+// app.get('/users/:id', db.getUserById);
+// app.post('/users', db.CreateUser);
+// app.put('/users/:id', db.updateUser);
+// app.delete('/users/:id', db.deleteUser);
+
